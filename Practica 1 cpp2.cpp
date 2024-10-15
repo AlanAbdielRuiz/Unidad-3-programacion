a945e4e5fc162fb087f988517404142825c97910
AlanAbdielRuiz <abdielmaran@gmail.com>
2024-10-15 11:38:56 -0600

    Add files via upload

---
+++ Practica 1 cpp2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int EDAD;
+	cout<<"¿que edad tienes?\n";
+	cin>>EDAD;
+	if(EDAD>20)
+	{cout<<" Eres mayor de edad";
+	cout<<" Te estas volviendo viejo";
+	}
+}
